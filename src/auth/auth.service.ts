0026663cb501dc4152a305a0811acf926009a68f
trantienthuong <thuongtt@7saturday.com>
2020-05-04 11:26:48 +0700

    hash password and sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
@@ -13,5 +14,13 @@ export class AuthService {
   }
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return await this.userRepository.signUp(authCredentialsDto);
+  }
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+    // console.log('username', username);
+    if (!username) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
   }
 }
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany } from "typeorm";
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 @Unique(['username']) //xử lí các vấn đề liên quan đến trùng lặp user
@@ -41,5 +42,9 @@ export class User extends BaseEntity {
 
   @Column()
   position:string;
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 
 }
